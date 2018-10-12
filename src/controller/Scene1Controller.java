d31070662adac7849b1a4a15f9e08e614028874a
Honriri <632858620@qq.com>
2018-10-12 18:26:48 +0200

    Initial commit

---
+++ src/controller/Scene1Controller.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Scene1Controller {
+
+    public void nextScene(ActionEvent event) throws IOException
+    {
+        Parent scene2 = FXMLLoader.load(getClass().getResource("../view/scene2.fxml"));
+        Scene editCharaScene = new Scene(scene2);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(editCharaScene);
+        window.show();
+    }
+}
---
+++ src/controller/Scene2Controller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Scene2Controller {
+
+    public void nextScene(ActionEvent event) throws IOException
+    {
+        Parent scene3 = FXMLLoader.load(getClass().getResource("../view/scene3.fxml"));
+        Scene diffScene = new Scene(scene3);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(diffScene);
+        window.show();
+    }
+
+    public void backScene(ActionEvent event) throws IOException
+    {
+        Parent scene1 = FXMLLoader.load(getClass().getResource("../view/scene1.fxml"));
+        Scene selectCharaScene = new Scene(scene1);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(selectCharaScene);
+        window.show();
+    }
+}
