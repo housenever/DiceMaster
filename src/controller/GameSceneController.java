196414b56c3976f422de3cb2f8fbff787d720c6d
Honriri <632858620@qq.com>
2018-10-15 21:03:26 +0200

    完成dice生成随机数功能

--- src/controller/GameSceneController.java
+++ src/controller/GameSceneController.java
@@ -7,8 +7,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.DiceModel;
 import model.MapModel;
 import java.io.IOException;
 import java.net.URL;
@@ -19,7 +26,37 @@ public class GameSceneController implements Initializable {
 
     @FXML
     private Pane mazeMap;
+    @FXML
+    private ProgressBar bloodBar;
+    @FXML
+    private VBox sideBar;
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private ImageView charaPic;
+    @FXML
+    private HBox header;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private Label diceResult;
+    @FXML
+    private Button diceBtn;
+    @FXML
+    private Label userName;
+    @FXML
+    private VBox operateArea;
+    @FXML
+    private VBox userProfile;
 
+    @FXML
+    public void rollDice(ActionEvent event) {
+        DiceModel diceModel = new DiceModel();
+        int currentNumber = diceModel.rollDice();
+        diceResult.setText(String.valueOf(currentNumber));
+    }
+
+    @FXML
     public void exitGame(ActionEvent event) throws IOException
     {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
--- src/model/DiceModel.java
+++ src/model/DiceModel.java
@@ -1,13 +1,22 @@
 package model;
 
 public class DiceModel {
-	
+
 	private int diceNumber;
+
+	public int getDiceNumber() {
+		return diceNumber;
+	}
+
+	public void setDiceNumber(int diceNumber) {
+		this.diceNumber = diceNumber;
+	}
 	
 	public int rollDice() {
 		double d = Math.random();
 		int r = (int) ((d * 10) % 6 + 1);
 		diceNumber = r;
+		setDiceNumber(diceNumber);
 		return diceNumber;
 	}
 
